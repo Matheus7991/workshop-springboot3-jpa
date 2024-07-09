a3db8951af9128720c938a8155ea17d0195e63e0
Matheus Freitas <bandeira.freitas2013@gmail.com>
2024-07-09 19:04:50 -0300

    Subtotal & Total methods

--- src/main/java/com/freitasmat/course/entities/Order.java
+++ src/main/java/com/freitasmat/course/entities/Order.java
@@ -80,11 +80,6 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}	
-
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
@@ -107,6 +102,19 @@ public class Order implements Serializable{
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}	
 
 	@Override
 	public boolean equals(Object obj) {
